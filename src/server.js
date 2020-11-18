0021e715245a8001f1d6a61e317356d4a4b2467c
Sami Honkanen <sami.honkanen@live.fi>
2020-11-18 19:09:55 +0200

    basic express server and project deps

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.status(200).send("Kaikki kunnossa!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
